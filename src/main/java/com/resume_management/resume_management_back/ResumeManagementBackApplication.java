779fd4a78137bc2c11a7e036c5cafde9ef72adae
ehy0414 <djagusdyd78@gmail.com>
2024-09-08 17:32:30 +0900

    Spring 프로젝트 생성

---
+++ src/main/java/com/resume_management/resume_management_back/ResumeManagementBackApplication.java
@@ -0,0 +1,13 @@
+package com.resume_management.resume_management_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeManagementBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeManagementBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resume_management/resume_management_back/ResumeManagementBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume_management.resume_management_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeManagementBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
