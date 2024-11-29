40303ce7321335a6ddfefd320c20fda09cc8ecd5
Gunzzal <rkdrjsgks1508@gmail.com>
2024-11-29 14:31:36 +0900

    데이터베이스 연결

--- src/main/java/com/resume_management/resume_management_back/config/WebConfig.java
+++ src/main/java/com/resume_management/resume_management_back/config/WebConfig.java
@@ -2,16 +2,17 @@ package com.resume_management.resume_management_back.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
-    
 }
