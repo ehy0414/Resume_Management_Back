ed48850340f75f47572a5df38e30f42a3c68d843
ehy0414 <djagusdyd78@gmail.com>
2024-11-29 15:39:33 +0900

    회원가입 기능 수정

--- src/main/java/com/resume_management/resume_management_back/user/ctrl/UserController.java
+++ src/main/java/com/resume_management/resume_management_back/user/ctrl/UserController.java
@@ -1,35 +1,35 @@
-// package com.resume_management.resume_management_back.user.ctrl;
+package com.resume_management.resume_management_back.user.ctrl;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.resume_management.resume_management_back.user.dto.JoinRequestDTO;
-// import com.resume_management.resume_management_back.user.dto.JoinResponseDTO;
-// import com.resume_management.resume_management_back.user.service.UserService;
+import com.resume_management.resume_management_back.user.dto.JoinRequestDTO;
+import com.resume_management.resume_management_back.user.dto.JoinResponseDTO;
+import com.resume_management.resume_management_back.user.service.UserService;
 
-// import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-// @RestController
-// @RequestMapping("/users")
-// @RequiredArgsConstructor
-// public class UserController {
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
 
-//     private final UserService userService;
+    private final UserService userService;
 
-//     // 회원가입 & 로그인
-//     @PostMapping("/join")
-//     public ResponseEntity<JoinResponseDTO> join(@RequestBody JoinRequestDTO joinRequestDTO) {
-//         JoinResponseDTO result = userService.join(joinRequestDTO);
+    // 회원가입 & 로그인
+    @PostMapping("/join")
+    public ResponseEntity<JoinResponseDTO> join(@RequestBody JoinRequestDTO joinRequestDTO) {
+        JoinResponseDTO result = userService.join(joinRequestDTO);
 
-//         if (!result.getErrMsg().isEmpty()) {
-//             return new ResponseEntity<>(result, HttpStatus.CONFLICT);
-//         }
+        if (!result.getErrMsg().isEmpty()) {
+            return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+        }
 
-//         return new ResponseEntity<>(result, HttpStatus.OK);
-//     }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
     
-// }
+}
--- src/main/java/com/resume_management/resume_management_back/user/service/UserService.java
+++ src/main/java/com/resume_management/resume_management_back/user/service/UserService.java
@@ -1,32 +1,32 @@
-// package com.resume_management.resume_management_back.user.service;
+package com.resume_management.resume_management_back.user.service;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import com.resume_management.resume_management_back.user.dao.UserMapper;
-// import com.resume_management.resume_management_back.user.dto.JoinRequestDTO;
-// import com.resume_management.resume_management_back.user.dto.JoinResponseDTO;
+import com.resume_management.resume_management_back.user.dao.UserMapper;
+import com.resume_management.resume_management_back.user.dto.JoinRequestDTO;
+import com.resume_management.resume_management_back.user.dto.JoinResponseDTO;
 
-// import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-// @Service
-// @RequiredArgsConstructor
-// public class UserService {
-//     private final UserMapper userMapper;
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserMapper userMapper;
 
-//     public Boolean existsByEmail(String email) {
-//         return userMapper.existsByEmail(email);
-//     }
+    public Boolean existsByEmail(String email) {
+        return userMapper.existsByEmail(email);
+    }
 
-//     public JoinResponseDTO join(JoinRequestDTO joinDTO) {
-//         if (userMapper.existsByEmail(joinDTO.getEmail())) {
-//             return new JoinResponseDTO("동일한 이메일을 사용하는 계정이 이미 존재합니다.");
-//         }
+    public JoinResponseDTO join(JoinRequestDTO joinDTO) {
+        if (userMapper.existsByEmail(joinDTO.getEmail())) {
+            return new JoinResponseDTO("동일한 이메일을 사용하는 계정이 이미 존재합니다.");
+        }
 
-//         joinDTO.setPassword(joinDTO.getPassword());
-//         // joinDTO.setRole("ROLE_BABONE");
+        joinDTO.setPassword(joinDTO.getPassword());
+        // joinDTO.setRole("ROLE_BABONE");
 
-//         userMapper.join(joinDTO);
+        userMapper.join(joinDTO);
 
-//         return new JoinResponseDTO("");
-//     }
-// }
+        return new JoinResponseDTO("");
+    }
+}
